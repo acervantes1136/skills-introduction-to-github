5b7f3772bf313ff3df9b3c2fe8a60919b6c2a6f6
acervantes1136 <acervantes1136@students.mchenry.edu>
2026-01-26 22:30:52 -0600

    Add files via upload

---
+++ character.py
@@ -0,0 +1,22 @@
+"""
+-----------------------------------------------------------------------
+ASSIGNMENT REQUIREMENTS (DO NOT DELETE)
+-----------------------------------------------------------------------
+[ ] 1. Header Docstring included.
+[ ] 2. Define a String variable.
+[ ] 3. Define an Integer variable.
+[ ] 4. Define a Float variable.
+[ ] 5. Define a Boolean variable.
+[ ] 6. Print all variables using F-Strings.
+[ ] 7. Upload to GitHub.
+-----------------------------------------------------------------------
+"""
+
+# How much an apple weighs
+fruit = "Apple"
+quantity = 1
+weight_pounds = 0.33
+on_sale = True
+
+print(f"{quantity} {fruit} weighs {weight_pounds} pounds")
+print(f"They're on sale? {on_sale}")
