8c8613c487c38c8657f93e36046bb7456931be90
acervantes1136 <acervantes1136@students.mchenry.edu>
2026-03-15 18:45:35 -0500

    Add files via upload

---
+++ stub.py
@@ -0,0 +1,29 @@
+"""
+-----------------------------------------------------------------------
+ASSIGNMENT Mid-Project Audit: HOTEL BOOKING
+-----------------------------------------------------------------------
+[ ] 1. Ask the user for their age
+[ ] 2. Ask how many people are staying and for how long (# of nights)
+[ ] 3. Asks the user for personal information (First&Last name, Email, and Phone Number
+[ ] 4. Asks the user if they have read the cancellation policy/understand it
+[ ] 5. The program should have two different messages for user age and understanding policy (Yes & No)
+-----------------------------------------------------------------------
+"""
+def get_positive_number(prompt):
+    # Makes sure user enters a positive whole number
+
+
+def hotel_booking():
+    # Checks user age
+    # TODO: Ask for the users age to ensure it aligns with hotel 21+ age policy
+    #Use if else
+
+# TODO: Get users information to book hotel room (First/Last name, emails, number, nights stayed, # of people)
+
+# TODO: Ask user about cancellation policy (Yes or No)
+# Use if else
+
+# TODO: Create a Raw Data Log to then save the hotel booking on file
+
+""" Human report """
+# TODO: Print out the users inputs and ask if they understand the cancellation policy
